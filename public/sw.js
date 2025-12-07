189334576cf051766ed24010b09e9d33e84ef53e
Robert Dawson <manatees@gmail.com>
2025-12-06 16:18:23 -0800

    feat: Rebrand to TipKarma and add PWA support

---
+++ public/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'tipkarma-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+  // Vite bundles (js/css) will change names, so we rely on runtime caching or network-first for those in a simple setup
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
